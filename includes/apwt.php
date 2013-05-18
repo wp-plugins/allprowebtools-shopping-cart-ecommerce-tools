a1f76fd0190b8c4dfbbaebb2701f5cd7097d2493
allprowebtools <allprowebtools@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-18 18:59:45 +0000

    created 0.45 BETA tag
    git-svn-id: https://plugins.svn.wordpress.org/allprowebtools-shopping-cart-ecommerce-tools/tags/0.45 BETA@715041 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/apwt.php
@@ -0,0 +1,409 @@
+<?php
+
+//master apwt functions (generally used in all other functions and includes)
+
+function APIAuth() {
+	$apikey = get_option("APWTAPIKEY");
+	$apiauth = get_option("APWTAPIAUTH");
+
+	if ( ($apiauth == '') || ($apikey == '') ) {
+		print "The AllProWebTools plugin has not been configured.  Please enter API Credential in wp-admin under 'AllProWebTools -> Settings'.";
+		exit;
+	}
+
+	return "apwtpluginversion=".urlencode(get_option("APWT_THIS_VERSION"))."&apikey=".get_option("APWTAPIKEY")."&apiauth=".get_option("APWTAPIAUTH");
+}
+
+function APWTGetUrl() {
+	$thisurl = "http://auth1.allprowebtools.com/listener/installinfo.php?".APIAuth();
+	$thereturn = wp_remote_get($thisurl);
+
+	if ($thereturn['body'] == 'API credentials not found') {
+		print $thereturn['body'];
+		exit;
+	}
+
+	return $thereturn['body'];
+}
+
+function init_sessions() {
+  if (!session_id()) {
+      session_start();
+  }
+}
+
+function APWTGetPageIDs() {
+	//gets all the pageids for the APWT pages
+	$pagelist = APWTPageList();
+
+	foreach ($pagelist as $var) {
+	  $the_page = get_site_option( $var['Title'] );
+	  if (!$the_page) {
+	  	print "lost page ".$var['Title']." cannot continue.";
+	  	exit;
+	  }
+	  $newarr[$var['Title']] = $the_page;
+	 }
+	 return $newarr;
+}
+
+function apwt_remote_post($url,$postvars,$secure=0) {
+	if ($_SESSION['serverurl'] == '') {
+		$_SESSION['serverurl'] = APWTGetUrl();
+	}
+
+	if ($_SESSION['serverurl'] == '') {
+		print "unable to contact auth server";
+		exit;
+	}
+
+	$debug = 0;
+	$args = cookiesessionhandler();
+	$args['timeout'] = 30;
+	$pagelist = APWTGetPageIDs();
+
+	foreach ($pagelist as $key => $value) {
+		$postvars['pagearray'][$key] = $value;
+	}
+
+	$url .= $urlappend;
+	if ($secure > 0 ) {
+		$protocol = 'https:';
+	} else {
+		$protocol = 'http:';
+	}
+	$args['body'] = $postvars;
+	$response = wp_remote_post($protocol.$_SESSION['serverurl'].$url, $args);
+
+	if (empty($response->errors)) {
+		//no errors found
+
+		$lastsessionname = $response['cookies'][0]->name;
+		if ($lastsessionname == 'PHPSESSID') {
+			$_SESSION['PHPSESSID'] = $response['cookies'][0]->value;
+		}
+		if ($debug > 0) {
+			print "<hr>".$url."<hr>";
+
+			print "<pre>";
+			print_r ($args);
+			print "</pre>";
+
+			print "<pre>";
+			print_r ($_SESSION);
+			print "</pre>";
+		}
+		return $response['body'];
+	} else {
+		print "<pre>";
+		print_r($response);
+		print "</pre>";
+		print "error in apwt_remote_get";
+		exit;
+	}
+}
+
+function apwt_remote_get($url) {
+	if ($_SESSION['serverurl'] == '') {
+		$_SESSION['serverurl'] = APWTGetUrl();
+	}
+
+	if ($_SESSION['serverurl'] == '') {
+		print "unable to contact auth server";
+		exit;
+	}
+
+	$debug = 0;
+	$args = cookiesessionhandler();
+	$args['timeout'] = 30;
+	$pagelist = APWTGetPageIDs();
+
+	foreach ($pagelist as $key => $value) {
+		$urlappend .= "&pagearray[".$key."]=".$value;
+	}
+	$url .= $urlappend;
+	$response = wp_remote_get("http:".$_SESSION['serverurl'].$url, $args);
+
+	if (empty($response->errors)) {
+		//no errors found
+
+		$lastsessionname = $response['cookies'][0]->name;
+		if ($lastsessionname == 'PHPSESSID') {
+			$_SESSION['PHPSESSID'] = $response['cookies'][0]->value;
+		}
+		if ($debug > 0) {
+			print "<hr>".$url."<hr>";
+
+			print "<pre>";
+			print_r ($args);
+			print "</pre>";
+
+			print "<pre>";
+			print_r ($_SESSION);
+			print "</pre>";
+		}
+		return $response['body'];
+	} else {
+		print "<pre>";
+		print_r($response);
+		print "</pre>";
+		print "error in apwt_remote_get";
+		exit;
+	}
+}
+
+function cookiesessionhandler() {
+	if ($_SESSION['PHPSESSID'] != '') {
+	  $cookie = new WP_Http_Cookie( 'PHPSESSID' );
+	  $cookie->name = 'PHPSESSID';
+	  $cookie->value = $_SESSION['PHPSESSID'];
+	  $cookie->expires = mktime( 0, 0, 0, date('m'), date('d') + 7, date('Y') ); // expires in 7 days
+	  $cookie->path = '/';
+	  $cookie->domain = '';
+
+	  $cookies[] = $cookie;
+		$args = array(  'cookies' => $cookies );
+	}
+	return $args;
+}
+
+
+function APWTCartActivate() {
+//http://wordpress.org/support/topic/how-do-i-create-a-new-page-with-the-plugin-im-building
+	$APWTThisVersion = APWT_plugin_get_version();
+	update_option("APWT_THIS_VERSION", $APWTThisVersion);
+
+	$APWTPageArray = APWTPageList();
+
+	foreach ($APWTPageArray as $APWTVar) {
+		APWTAddPage($APWTVar['Title'],$APWTVar['URL'],$APWTVar['Content'],$APWTVar['Template']);
+	}
+}
+
+function APWTCartDeactivate() {
+	$APWTPageArray = APWTPageList();
+
+	foreach ($APWTPageArray as $APWTVar) {
+		APWTRemovePage($APWTVar['Title']);
+	}
+}
+
+function APWTRemovePage($the_page_title) {
+
+  global $wpdb;
+
+  $the_page = get_page_by_title( $the_page_title );
+  //  the id of our page...
+  $the_page_id = $the_page->ID;
+  if( $the_page_id ) {
+    wp_delete_post( $the_page_id ); // this will trash, not delete
+  }
+}
+
+function APWTAddPage($the_page_title,$the_page_name,$content,$template='') {
+
+    global $wpdb;
+
+    $the_page = get_page_by_title( $the_page_title );
+
+    if ( ! $the_page ) {
+
+        // Create post object
+        $_p = array();
+        $_p['post_title'] = $the_page_title;
+        $_p['post_content'] = $content;
+        $_p['post_status'] = 'publish';
+        $_p['post_type'] = 'page';
+        $_p['comment_status'] = 'closed';
+        $_p['ping_status'] = 'closed';
+        $_p['post_category'] = array(1); // the default 'Uncatrgorised'
+
+        // Insert the post into the database
+        $the_page_id = wp_insert_post( $_p );
+
+    }
+    else {
+        // the plugin may have been previously active and the page may just be trashed...
+
+        $the_page_id = $the_page->ID;
+
+        //make sure the page is not trashed...
+        $the_page->post_status = 'publish';
+        $the_page_id = wp_update_post( $the_page );
+
+    }
+    update_site_option( $the_page_title, $the_page_id );
+    if ($template != '') {
+    	//we need to apply a specific template
+    	update_post_meta($the_page_id, "_wp_page_template", $template);
+    }
+}
+
+function APWTPageList() {
+
+	$APWTPageArray[0]['Title'] = 'APWTListProducts';
+	$APWTPageArray[0]['Content'] = '[APWTListProducts]';
+	$APWTPageArray[0]['URL'] = 'APWTListProducts';
+	$APWTPageArray[0]['Template'] = '';
+
+	$APWTPageArray[1]['Title'] = 'APWTForgotPassword';
+	$APWTPageArray[1]['Content'] = '[APWTForgotPassword]';
+	$APWTPageArray[1]['URL'] = 'APWTForgotPassword';
+	$APWTPageArray[1]['Template'] = '';
+
+	$APWTPageArray[2]['Title'] = 'APWTCart';
+	$APWTPageArray[2]['Content'] = '[APWTShowCart]';
+	$APWTPageArray[2]['URL'] = 'APWTCart';
+	$APWTPageArray[2]['Template'] = '';
+
+	$APWTPageArray[3]['Title'] = 'APWTCheckout';
+	$APWTPageArray[3]['Content'] = '[APWTCheckout]';
+	$APWTPageArray[3]['URL'] = 'APWTCheckout';
+	$APWTPageArray[3]['Template'] = 'page-templates/full-width.php';
+
+	$APWTPageArray[4]['Title'] = 'APWTLogout';
+	$APWTPageArray[4]['Content'] = '[APWTLogout]';
+	$APWTPageArray[4]['URL'] = 'APWTLogout';
+	$APWTPageArray[4]['Template'] = '';
+
+	$APWTPageArray[5]['Title'] = 'APWTShowProduct';
+	$APWTPageArray[5]['Content'] = '[APWTShowProduct]';
+	$APWTPageArray[5]['URL'] = 'APWTShowProduct';
+	$APWTPageArray[5]['Template'] = '';
+
+	$APWTPageArray[6]['Title'] = 'APWTAddReview';
+	$APWTPageArray[6]['Content'] = '[APWTAddReview]';
+	$APWTPageArray[6]['URL'] = 'APWTAddReview';
+	$APWTPageArray[6]['Template'] = '';
+
+	$APWTPageArray[7]['Title'] = 'APWTThankYou';
+	$APWTPageArray[7]['Content'] = '[APWTThankYou]';
+	$APWTPageArray[7]['URL'] = 'APWTThankYou';
+	$APWTPageArray[7]['Template'] = '';
+
+	return $APWTPageArray;
+}
+
+function APWTHitTracker() {
+	$apikey = get_option("APWTSITEID");	//someday we can define this in the WP settings
+	if ($apikey == '') {
+		$apikey = 'wp1';
+	}
+
+	@$thisurl = '/wordpress/wpapi.php?action=iptrackhit&ip='.$_SERVER["REMOTE_ADDR"].'&siteid='.$apikey.'&aff='.$_GET['aff'].'&refcode='.$_GET['ref'].'&'.APIAuth();
+	$thereturn = apwt_remote_get($thisurl);
+}
+
+////////////////////////////// Begin Classes /////////////////////////////////
+
+class APWTLeadBox extends WP_Widget {
+//http://azuliadesigns.com/create-wordpress-widgets/
+  function APWTLeadBox() {
+    $widget_ops = array('classname' => 'APWTLeadBox', 'description' => 'Displays an APWT LeadBox Widget' );
+    $this->WP_Widget('APWTLeadBox', 'APWT LeadBox Widget', $widget_ops);
+  }
+
+  function form($instance) {
+  //get a title variable defined by the user
+    $instance = wp_parse_args( (array) $instance, array( 'APWTLeadBoxID' => '' ) );
+    $APWTLeadBoxID = $instance['APWTLeadBoxID'];
+    //get the ids for the lead box
+		$thisurl = "/wordpress/wpapi.php?action=leadboxids&".APIAuth();
+		$thereturn = apwt_remote_get($thisurl);
+
+		$leadboxes = explode("-|-",$thereturn);
+		array_pop($leadboxes);
+
+		foreach ($leadboxes as $val) {
+			$pieces = explode("-*-",$val);
+			$newarray[$pieces[0]] = $pieces[1];
+		}
+    $APWTLeadBoxID = empty($instance['APWTLeadBoxID']) ? ' ' : apply_filters('widget_title', $instance['APWTLeadBoxID']);
+
+?>
+  <p><label for="<?php echo $this->get_field_id('APWTLeadBoxID'); ?>">Lead Box:
+<?php
+		//create select box
+		print '<select id="'.$this->get_field_id('APWTLeadBoxID').'" name="'.$this->get_field_name('APWTLeadBoxID').'">';
+		foreach ($newarray as $lbid => $lbname) {
+			if ($APWTLeadBoxID == $lbid) {
+				$selected = "SELECTED";
+			} else {
+				$selected = "";
+			}
+			print '<option value="'.$lbid.'" '.$selected.'>'.$lbname.'</option>';
+		}
+		print '</select>';
+?>
+	</label></p>
+<?php
+  }
+
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['APWTLeadBoxID'] = $new_instance['APWTLeadBoxID'];
+    return $instance;
+  }
+
+  function widget($args, $instance) {
+    extract($args, EXTR_SKIP);
+    echo $before_widget;
+		if (isset($_POST['APWTLeadBox'])) {
+    $APWTLeadBoxID = empty($instance['APWTLeadBoxID']) ? ' ' : apply_filters('widget_title', $instance['APWTLeadBoxID']);
+			$thisurl = "/wordpress/wpapi.php?action=getleadbox&leadboxid=".$APWTLeadBoxID."&name=".$_POST['APWTName']."&email=".$_POST['APWTEmail']."&".APIAuth();
+			$thereturn = apwt_remote_get($thisurl);
+			echo $thereturn;
+
+		} else {
+    $APWTLeadBoxID = empty($instance['APWTLeadBoxID']) ? ' ' : apply_filters('widget_title', $instance['APWTLeadBoxID']);
+
+    if ($APWTLeadBoxID > 0) {
+			$thisurl = "/wordpress/wpapi.php?action=getleadbox&leadboxid=".$APWTLeadBoxID."&".APIAuth();
+			$thereturn = apwt_remote_get($thisurl);
+
+      echo $before_title . $thereturn . $after_title;
+		} else {
+	    echo "<h1>No Leadbox defined</h1>";
+	  }
+	}
+    echo $after_widget;
+  }
+} //end class extend APWTLeadBox
+
+class APWTCartWidget extends WP_Widget {
+//http://azuliadesigns.com/create-wordpress-widgets/
+  function APWTCartWidget()
+  {
+    $widget_ops = array('classname' => 'APWTCartWidget', 'description' => 'Displays the APWT Cart Widget' );
+    $this->WP_Widget('APWTCartWidget', 'APWT Cart Widget', $widget_ops);
+  }
+
+  function form($instance) {
+  //get a title variable defined by the user
+    $instance = wp_parse_args( (array) $instance, array( 'APWTCartWidget' => '' ) );
+    $APWTCartWidget = $instance['APWTCartWidget'];
+?>
+  <p><label for="<?php echo $this->get_field_id('APWTCartWidget'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('APWTCartWidget'); ?>" name="<?php echo $this->get_field_name('APWTCartWidget'); ?>" type="text" value="<?php echo attribute_escape($APWTCartWidget); ?>" /></label></p>
+<?php
+  }
+
+  function update($new_instance, $old_instance) {
+    $instance = $old_instance;
+    $instance['APWTCartWidget'] = $new_instance['APWTCartWidget'];
+    return $instance;
+  }
+
+  function widget($args, $instance) {
+    extract($args, EXTR_SKIP);
+
+    $APWTCartWidget = empty($instance['APWTCartWidget']) ? ' ' : apply_filters('widget_title', $instance['APWTCartWidget']);
+    	print '<div class="cartwidget">';
+				print $APWTCartWidget."<br>";
+				$thisurl = "/wordpress/wpapi.php?action=cartwidget&".APIAuth();
+				$thereturn = apwt_remote_get($thisurl);
+				print $thereturn;
+			print '</div>';
+
+  }
+} //end class extend APWTCartWidget
+?>
---
+++ readme.txt
@@ -0,0 +1,92 @@
+=== Plugin Name ===
+Contributors: allprowebtools, pmschartz, rcgig, ceec
+Tags: allprowebtools, e-commerce, shop, cart, paypal, authorize, inventory, ecommerce, shipping, all, pro, tools
+Requires at least: 3.4
+Tested up to: 3.5.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A cloud-based shopping cart integrated with email marketing that can be shared across multiple devices and/or WordPress installs.
+
+== Description ==
+
+After 5 years as a standalone Ecommerce system, Take a test spin of our WordPress plugin BETA.  We would love to hear your feedback!
+
+* iPhone integration with the [AllProWebTools iPhone app](http://www.allprowebtools.com/app)
+* Shopping Cart
+* Extensive Visitor Tracking - How many sales dollars were generated from a specific email ad or newsletter.
+* Integrated Email Marketing - Automatically offer a coupon to abandoned carts.
+* Physical & Downloadable Products
+* Phone Orders - Track sales from phone calls and how they originated online.
+* Coupons & Discounts
+* Integrated Shipping Labels - Print shipping right from your control panel and leave the package at the door to be picked up.
+* Customized PDF Invoices
+* Product Upsell
+* Data Exports
+* Authorize.Net and PayPal Integration
+* Help videos are included on each page of the control panel
+* Shipping methods - USPS, FedEx, UPS
+
+This is a Serviceware plugin for WordPress which interfaces with the AllProWebTools cloud servers to process transactions, store inventory information, customer data, and sales and visitor analytics.  Storing this information in the cloud allows all of your devices and websites to access this information and increase exposure of your products and services.  An active AllProWebTools account is required to use this plugin a link will be provided to create a new account from within the plugin settings menu.
+
+== Installation ==
+
+**Automatic Installation**
+
+1. Login to your wordpress with wp-admin
+1. Choose **Plugins** from the menu and click **Add New**
+1. Search for **AllProWebTools** and then click **Install Now**
+1. Once the plugin is installed, click **Activate Now**
+1. Next see **Configuration** below.
+
+**Manual Installation**
+
+1. Unzip `allprowebtools.zip` to the `/wp-content/plugins/` directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+
+**Configuration**
+
+1. Upon activation, all of the necessary pages will be created to run the cart.
+1. Choose **settings** from the **AllProWebTools** menu in wp-admin and click the link to create an AllProWebTools account.
+1. Enter the API keys you recieve via email after creating your account and click **Update Settings**.
+1. Click the **AllProWebTools** link in the menu of wp-admin and click the link at the top of the page to access your AllProWebTools console and begin configuring your products.
+
+[AllProWebTools Tutorial videos](http://www.youtube.com/allprowebtools1) are available to assist you.
+
+== Frequently Asked Questions ==
+
+= Why do I see all these flowers when I first install? =
+
+New installs are setup with our demo account so you can see the functionality.  This also demonstrates how to tie multiple sites together, the demo site your new install is tied to is http://demo.AllProWebTools.com.  You can login to the demo account to play around in the control panel at http://demo.AllProWebTools.com/logmein. Login: demo Password: demo.  The demo resets itself every hour on the hour at which time your changes will be lost.
+
+= Which themes are compatible with this plugin? =
+
+We are still in the process of testing themes, a list will be made available.
+
+= Where is all of my product information stored? =
+
+All product information including inventory counts is stored on your cloud account with AllProWebTools.  This is what allows all smart phones, point of sale devices, and linked websites to share inventory information.  The benefit is that changing the retail price or uploading a new product can be done from your control panel and instantly update all devices.
+
+= Can I upload my inventory as a CSV? =
+
+You can upload or download all of your inventory in CSV format at any time.
+
+== Screenshots ==
+
+1. The shopping cart.
+2. Product Listing.
+3. Product Management Listing.
+4. Product Management Product Options.
+5. Product Management Quantity Discount.
+6. Point of sale
+7. Mobile Integration
+
+== Changelog ==
+
+= 0.44 [BETA] - 05/11/13 =
+1. addressed several issues with hard references to plugin dir
+1. improved some variable names for compatiblity
+
+= 0.43 [BETA] - 05/11/13 =
+1. first load to WP repository
