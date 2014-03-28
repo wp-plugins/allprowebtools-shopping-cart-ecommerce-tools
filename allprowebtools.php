5aeb5aa51cf5422f0547fc699758904ff237a44d
allprowebtools <allprowebtools@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-28 21:46:11 +0000

    New version commit
    git-svn-id: https://plugins.svn.wordpress.org/allprowebtools-shopping-cart-ecommerce-tools/trunk@883788 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- allprowebtools.php
+++ allprowebtools.php
@@ -3,7 +3,7 @@
 Plugin Name: AllProWebTools Cart
 Plugin URI: http://www.AllProWebTools.com
 Description: AllProWebTools Shopping Cart
-Version: 1.0.2
+Version: 1.0.3
 Author: AllProWebTools.com
 Author URI: http://www.AllProWebTools.com
 License: GPLv2
